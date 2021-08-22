f5d4366c4c1e91ccac764e06a219b1d9cfac9415
FantaGreap <radwimps07057505@gmail.com>
2021-08-23 00:23:31 +0900

    shuma 0823 0023

---
+++ Youtube_sugoroku/Assets/Script/grid_connection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class grid_connection : MonoBehaviour
+{
+    [SerializeField] int connection_number = 1;
+    public GameObject[] Station;
+    public GameObject Arrow;
+
+    
+}
