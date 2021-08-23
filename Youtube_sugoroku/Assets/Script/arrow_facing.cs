a8574298159f9e6fabb52b741371bf511e9305ec
FantaGreap <radwimps07057505@gmail.com>
2021-08-23 20:00:37 +0900

    shuma 0823 2000

---
+++ Youtube_sugoroku/Assets/Script/arrow_facing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrow_facing : MonoBehaviour
+{
+
+    [SerializeField] GameObject station;
+    private void OnValidate()
+    {
+        
+    }
+}
--- Youtube_sugoroku/Assets/Script/grid_connection.cs
+++ Youtube_sugoroku/Assets/Script/grid_connection.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿using UnityEngine;
+
 
 public class grid_connection : MonoBehaviour
 {
+    [SerializeField] int connection_number;
     public GameObject[] Station = new GameObject[4];
-    public GameObject Arrow;
+    [SerializeField] GameObject Arrow;
+    public GameObject ArrowPrefab;
 
-    
+    private void OnValidate()
+    {
+        foreach (Transform Arrow_Destroy in transform) Destroy(Arrow_Destroy);
+        int i;
+        Debug.Log("呼ばれたよ");
+        for (i = 0; i < connection_number; i++)
+        {
+            Vector3 station_potision = Station[i].transform.position;
+            Arrow = Instantiate(ArrowPrefab, new Vector3(station_potision.x - transform.position.x, 0.8f, station_potision.z - transform.position.z),
+                Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z));
+            Arrow.transform.parent = this.transform;
+        }
+        
+    }
+
 }
