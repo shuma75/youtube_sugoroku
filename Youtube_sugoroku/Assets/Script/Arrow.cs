b658cee452d65820c1580fe8b3422f4abb5ceeb2
FantaGreap <radwimps07057505@gmail.com>
2021-08-26 01:57:10 +0900

    shuma 0826 0156

--- Youtube_sugoroku/Assets/Script/Arrow.cs
+++ Youtube_sugoroku/Assets/Script/Arrow.cs
@@ -6,12 +6,19 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] grid_connection station;
     [SerializeField] int Station_Number = 1;
+    Player_Motion Next_Station;
 
     public grid_connection grid_Connection { get { return station; } set { station = value; } }
     public int station_Number { get { return Station_Number; } set { Station_Number = value; } }
 
+    private void Start()
+    {
+        
+    }
+
     public void Click()
     {
         Debug.Log(station.station[Station_Number - 1]);
+        Next_Station.next_Station = GameObject.Find(station.station[Station_Number - 1]);
     }
 }
--- Youtube_sugoroku/Assets/Script/Player_Motion.cs
+++ Youtube_sugoroku/Assets/Script/Player_Motion.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player_Motion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float Speed = 2;
+    [HideInInspector] public GameObject next_Station;
+
+    private void Start()
     {
-        
+        next_Station.transform.position = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position - new Vector3(next_Station.transform.position.x, 0.3f, next_Station.transform.position.z) != Vector3.zero)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(next_Station.transform.position.x, 0.3f, next_Station.transform.position.z), Speed * Time.deltaTime);
+        }
+        Debug.Log(next_Station.transform.position);
     }
 }
